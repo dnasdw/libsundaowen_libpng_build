d4d2b146f95996922571ec3b9dc9a3fa149c0081
Sun Daowen <sundaowen@gmail.com>
2016-11-20 22:21:38 +0800

    linux

---
+++ libpng/make_macos_x86_64.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cwdir=`pwd`
+rootdir=`dirname $0`
+cd "$rootdir"
+rootdir=`pwd`
+target=macos_x86_64
+prefix=$rootdir/$target
+version=`cat $rootdir/version.txt`
+rm -rf "$rootdir/$version"
+mkdir "$rootdir/$version"
+cp -rf "$rootdir/../$version/"* "$rootdir/$version"
+rm -rf "$rootdir/project"
+mkdir "$rootdir/project"
+cd "$rootdir/project"
+cmake -DBUILD64=ON -C "$rootdir/CMakeLists.txt" -DPNG_SHARED=OFF -DPNG_TESTS=OFF -DSKIP_INSTALL_FILES=ON -DSKIP_INSTALL_EXPORT=ON -DZLIB_INCLUDE_DIR="$rootdir/../zlib/include/$target" -DZLIB_LIBRARY="$rootdir/../zlib/lib/$target/libz.a" -DM_LIBRARY="${M_LIBRARY};/usr/lib32" -DCMAKE_INSTALL_PREFIX="$prefix" "$rootdir/$version"
+cmake "$rootdir/$version"
+cmake --build . --target install --config Release --clean-first
+rm -rf "$prefix/include/libpng16"
+mkdir "$rootdir/../target/include/$target"
+cp -rf "$prefix/include/"* "$rootdir/../target/include/$target"
+mkdir "$rootdir/../target/lib/$target"
+cp -rf "$prefix/lib/"* "$rootdir/../target/lib/$target"
+cd "$cwdir"
+rm -rf "$rootdir/$version"
+rm -rf "$rootdir/project"
+rm -rf "$prefix"
